4e266b188279f9203e09b7675e21f748a6356147
mathooo <trojak@mail.muni.cz>
2015-11-11 14:30:13 +0100

    wrong commit, added binary file...

--- main.cpp
+++ main.cpp
@@ -27,16 +27,20 @@ f64 dsigmoidal(f64 x)
 
 int main(int argc, char **argv)
 {
-    Net n = Net(std::vector<u32> {2, 2, 1}, &sigmoidal, &dsigmoidal);
+    Net n = Net(std::vector<u32> {3, 2, 1}, &sigmoidal, &dsigmoidal);
 
     n.print();
     cout << "\n";
 
     vector< tuple< vector<f64>, vector<f64> >> data = {
-        make_tuple(vector<f64> {1,1}, vector<f64> {1}),
-        make_tuple(vector<f64> {0,0}, vector<f64> {0}),
-        make_tuple(vector<f64> {1,0}, vector<f64> {0}),
-        make_tuple(vector<f64> {0,1}, vector<f64> {0})
+        make_tuple(vector<f64> {1,1,1}, vector<f64> {1}),
+        make_tuple(vector<f64> {1,1,0}, vector<f64> {0}),
+        make_tuple(vector<f64> {1,0,1}, vector<f64> {0}),
+        make_tuple(vector<f64> {1,0,0}, vector<f64> {0}),
+        make_tuple(vector<f64> {0,1,1}, vector<f64> {0}),
+        make_tuple(vector<f64> {0,1,0}, vector<f64> {0}),
+        make_tuple(vector<f64> {0,0,1}, vector<f64> {0}),
+        make_tuple(vector<f64> {0,0,0}, vector<f64> {0})
     };
 //    vector< tuple< vector<f64>, vector<f64> >> data = {
 //        make_tuple(vector<f64> {1,1}, vector<f64> {0}),
@@ -45,7 +49,7 @@ int main(int argc, char **argv)
 //        make_tuple(vector<f64> {0,1}, vector<f64> {1})
 //    };
 
-    for (u32 i = 0; i < 1000; i++)
+    for (u32 i = 0; i < 500; i++)
         n = backtrack(data, n);
 
     n.print();
@@ -55,10 +59,16 @@ int main(int argc, char **argv)
 //    n.getLayer(0)[1].setWeights(std::vector<f64> {3, -2, -2});
 //    n.getLayer(1)[0].setWeights(std::vector<f64> {-2, 1, 1});
 //    
-    cout << "(0, 0) -> " << n.eval(std::vector<f64> {0, 0})[0] <<
-        "\n(0, 1) -> " << n.eval(std::vector<f64> {0, 1})[0] <<
-        "\n(1, 0) -> " << n.eval(std::vector<f64> {1, 0})[0] <<
-        "\n(1, 1) -> " << n.eval(std::vector<f64> {1, 1})[0];
+    cout << "(1, 1, 1) -> " << n.eval(std::vector<f64> {1, 1, 1})[0] <<
+        "\n(1, 1, 0) -> " << n.eval(std::vector<f64> {1, 1, 0})[0] <<
+        "\n(1, 0, 1) -> " << n.eval(std::vector<f64> {1, 0, 1})[0] <<
+        "\n(1, 0, 0) -> " << n.eval(std::vector<f64> {1, 0, 0})[0] <<
+        "\n(0, 1, 1) -> " << n.eval(std::vector<f64> {0, 1, 1})[0] <<
+        "\n(0, 1, 0) -> " << n.eval(std::vector<f64> {0, 1, 0})[0] <<
+        "\n(0, 0, 1) -> " << n.eval(std::vector<f64> {0, 0, 1})[0] <<
+        "\n(0, 0, 0) -> " << n.eval(std::vector<f64> {0, 0, 0})[0];
+
+    cout << "\n";
 
 //    cout << "(0, 0) -> " << n.eval(std::vector<f64> {0, 0})[0] <<
 //        "\n(0, 1) -> " << n.eval(std::vector<f64> {0, 1})[0] <<
