f1a2c04e789155c20760286b4f0349dcefb20114
osense <krupicka.adam@gmail.com>
2015-12-01 10:29:38 +0100

    Add some helpful output when learning

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char **argv)
     {
         cout << "Example usage:\n\t" << argv[0] <<
             "\tnew \"2 3 3 2\" my.net\n\
-            \tteach chess.data my.net\n\
+            \tlearn chess.data my.net\n\
             \teval \"2 3 4 5 6 4 3 2 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 -1 -1 -1 -1 -1 -1 -1 -1 -2 -3 -4 -5 -6 -4 -3 -2\" my.net\n\n";
     }
     else if (string(argv[1]) == "new")
@@ -31,7 +31,7 @@ int main(int argc, char **argv)
         Net n(topology);
         n.serialize(argv[3]);
     }
-    else if (string(argv[1]) == "teach")
+    else if (string(argv[1]) == "learn")
     {
         Net n(argv[3]);
         Chess c(argv[2]);
@@ -43,6 +43,9 @@ int main(int argc, char **argv)
                 ideals.push_back(make_tuple(c.getMove(i).board, c.getMove(i).boardAfter));
         }
 
+        u32 example_count = ideals.size();
+        cout << "Learning on " << example_count << " examples.\n";
+
         Net newN = n;
         while (!ideals.empty())
         {
@@ -50,14 +53,20 @@ int main(int argc, char **argv)
             u32 dist = min(100u, u32(ideals.size()));
             is.splice(is.begin(), ideals, ideals.begin(), next(ideals.begin(), dist));
             newN = backprop(is, n);
+
+            cout << "\b\b\b\b" << 100-round((f64(ideals.size())/example_count)*100) << "%";
+            cout.flush();
         }
-        newN.serialize(argv[2]);
+        newN.serialize(argv[3]);
+        cout << endl;
     }
     else if (string(argv[1]) == "eval")
     {
         Net n(argv[3]);
         auto input = Chess::parseBoard(argv[2]);
         Chess::printBoard(input);
+        cout << endl;
+
         auto output = n.eval(input);
         Chess::printBoard(output);
     }
